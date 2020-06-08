1ee5844245dc482d6e6c9ae75a297a3e8966b02c
Gordon Schulz <gordon.schulz@gmail.com>
2020-06-08 09:53:03 +0200

    Username and uid in variables

--- ubuntu_zfs_firstboot.sh
+++ ubuntu_zfs_firstboot.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+user="azmo"
+uid="1337"
+
 # create home dataset and user
 UUID="$(dd if=/dev/urandom of=/dev/stdout bs=1 count=100 2>/dev/null |
     tr -dc 'a-z0-9' | cut -c-6)"
 ROOT_DS="$(zfs list -o name | awk '/ROOT\/ubuntu_/{print $1;exit}')"
 zfs create -o com.ubuntu.zsys:bootfs-datasets="${ROOT_DS}" \
-    -o canmount=on -o mountpoint=/home/azmo \
-    rpool/USERDATA/azmo_"${UUID}"
-adduser --uid 1337 azmo
-cp -a /etc/skel/. /home/azmo
-chown -R azmo:azmo /home/azmo
-usermod -a -G adm,cdrom,dip,lpadmin,lxd,plugdev,sambashare,sudo azmo
+    -o canmount=on -o mountpoint=/home/"${user}" \
+    rpool/USERDATA/"${user}"_"${UUID}"
+adduser --uid "${uid}" "${user}"
+cp -a /etc/skel/. /home/"${user}"
+chown -R "${user}":"${user}" /home/"${user}"
+usermod -a -G adm,cdrom,dip,lpadmin,lxd,plugdev,sambashare,sudo "${user}"
 
 # install standard cli environment
 apt-get install --yes ubuntu-standard
