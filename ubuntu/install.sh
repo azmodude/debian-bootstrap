fa80cd3685ba2309870e66f41d8793b61459168c
Gordon Schulz <gordon.schulz@gmail.com>
2020-06-12 10:57:33 +0200

    WIP
    Begin to work on getting Debian install to work

---
+++ ubuntu/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export UBUNTU_TREE="focal"
+export HOSTNAME_FQDN="test"
+export SWAP_SIZE="1"
+export ENCRYPTION_PASSPHRASE="testtest"
+export GRUB_PASSWORD=""
+export ROOT_PASSWORD="testtest"
+
+apt-get update && apt-get install --yes zfsutils-linux
+swapoff -a
+zpool destroy bpool || true
+zpool destroy rpool || true
+./ubuntu_zfs.sh
